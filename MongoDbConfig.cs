aa73049949eef0aa13b19efaf396e9b734cbf8d9
maj00r <januszj511@gmail.com>
2025-01-25 21:58:31 +0100

    credentials in connection string

--- MongoDbConfig.cs
+++ MongoDbConfig.cs
@@ -3,5 +3,9 @@ public class MongoDbConfig
     public required string Name { get; init; }
     public required string Host { get; init; }
     public int Port { get; init; }
-    public string ConnectionString => $"mongodb://{Host}:{Port}";
+    public required string Username { get; init; }
+    public required string Password { get; init; }
+    
+    public string ConnectionString => 
+        $"mongodb://{Username}:{Password}@{Host}:{Port}/{Name}";
 }
